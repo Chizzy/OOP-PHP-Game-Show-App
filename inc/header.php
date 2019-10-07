219084f66553d6aac5a53afac41a666201dac341
Chizzy <chizoba.princess@gmail.com>
2019-10-07 12:19:50 -0400

    declared and instantiated classes

--- inc/header.php
+++ inc/header.php
@@ -10,12 +10,3 @@
 	</head>
 
 	<body>
-		<div class="main-container">
-			<h2 class="header">Phrase Hunter</h2>
-            <form action="play.php">
-                <input id="btn__reset" type="submit" value="Start Game" />
-            </form>
-		</div>
-
-	</body>
-</html>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+require 'inc/header.php';
+
+?>
+
+<div class="main-container">
+	<h2 class="header">Phrase Hunter</h2>
+	<form action="play.php">
+		<input id="btn__reset" type="submit" value="Start Game" />
+	</form>
+</div>
+
+<?php
+
+require 'inc/footer.php';
